b645dc1a7b233a1788b5deb6169f99274a2eb3d6
vishal pathania <vishalpathania1210@gmail.com>
2025-09-23 18:19:55 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import LoginPage from "./Pages/LoginPage";
   <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />  
       </Routes>
     </Router>
     </>
