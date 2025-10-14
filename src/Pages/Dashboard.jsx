9bec0c4bda23f0db3bd300bda83e8f1d2bf45f71
vishalpathania1210 <vishalpathania1210@gmail.com>
2025-10-14 15:17:44 +0530

    added

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,10 +1,30 @@
 import NavBar from "../Components/NavBar";
 import SideBar from "../Components/SideBar";
-
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
  
  
 
 export default function Dashboard() {
+
+  const data = [
+    { name: 'Jan', Users: 4000 },
+    { name: 'Feb', Users: 3000 },
+    { name: 'Mar', Users: 2000 },
+    { name: 'Apr', Users: 2780 },
+    { name: 'May', Users: 1890 },
+    { name: 'Jun', Users: 2390 },
+    { name: 'Jul', Users: 3490 },
+  ];
+
   return (
     <div className="flex">
       <SideBar />
@@ -18,6 +38,18 @@ export default function Dashboard() {
             <div className="p-6 bg-white shadow rounded-lg">Card 1</div>
             <div className="p-6 bg-white shadow rounded-lg">Card 2</div>
             <div className="p-6 bg-white shadow rounded-lg">Card 3</div>
+          </div>
+          <div className="p-6 bg-white shadow rounded-lg h-64">
+<ResponsiveContainer width="100%" height="100%">
+<BarChart data={data}>
+<CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="Users" fill="#F2632D" />
+</BarChart>
+</ResponsiveContainer>
           </div>
         </div>
       </div>
